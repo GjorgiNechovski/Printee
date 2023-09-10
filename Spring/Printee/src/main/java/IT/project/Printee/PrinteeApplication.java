9fb7a829dd198d8b81f1d116880bbba3264fb374
Gjorgi Nechovski <gjorgi.nechovski@students.finki.ukim.mk>
2023-09-10 13:25:00 +0200

    Spring start files

---
+++ Spring/Printee/src/main/java/IT/project/Printee/PrinteeApplication.java
@@ -0,0 +1,13 @@
+package IT.project.Printee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinteeApplication.class, args);
+	}
+
+}
---
+++ Spring/Printee/src/test/java/IT/project/Printee/PrinteeApplicationTests.java
@@ -0,0 +1,13 @@
+package IT.project.Printee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
