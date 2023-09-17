6f4c34f4a844d20d72c9b84a2d97232f92ff7874
Gjorgi Nechovski <gjorgi.nechovski@students.finki.ukim.mk>
2023-09-17 16:03:01 +0200

    Log on in menu works

--- Printee/src/libs/feature/navigation/navigation/navigation.component.ts
+++ Printee/src/libs/feature/navigation/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { logo } from 'src/environment/appConfig';
 import { AuthenticationService } from '../../authentication/services/authentication.service';
 
@@ -7,10 +7,15 @@ import { AuthenticationService } from '../../authentication/services/authenticat
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  name? = '';
   constructor(private auth: AuthenticationService) {}
 
   logo = logo;
+
+  ngOnInit(): void {
+    this.name = this.auth.user?.name;
+  }
 
   logOut() {
     this.auth.logOut();
