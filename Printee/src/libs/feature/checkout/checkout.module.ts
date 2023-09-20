cd260b378a6de1ac787a8d42945581bbbaddd672
Gjorgi Nechovski <gjorgi.nechovski@students.finki.ukim.mk>
2023-09-20 20:30:16 +0200

    Generated checkout component

---
+++ Printee/src/libs/feature/checkout/checkout.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+
+@NgModule({
+  declarations: [CheckoutComponent],
+  imports: [CommonModule],
+})
+export class CheckoutModule {}
